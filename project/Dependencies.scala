ad99c9d2b40e1604e763932ed7b18b47580d236f
s-danilov <78469381+s-danilov@users.noreply.github.com>
2022-08-09 15:48:42 +0400

    exclude org.slf4j slf4j-api (#38)
    * fix: exclude org.slf4j slf4j-api
    
    * fix: scalafmt
    
    Co-authored-by: s.g.danilov <s.g.danilov@tinkoff.ru>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "io.gatling" % "gatling-test-framework" % gatlingVersion % "provided",
   )
 
-  lazy val hikari = "com.zaxxer"     % "HikariCP" % "5.0.1"
+  lazy val hikari = "com.zaxxer"     % "HikariCP" % "5.0.1" exclude ("org.slf4j", "slf4j-api")
   lazy val h2jdbc = "com.h2database" % "h2"       % "2.1.214" % Test
 
 }
