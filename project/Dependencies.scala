3ef4b21ea222aef876bffd8c5ca57a121b37b123
AlexandraPerevozchikova <74304626+AlexandraPerevozchikova@users.noreply.github.com>
2023-02-15 16:06:59 +0400

    fix: fix dependences and readme (#62)
    * feat: add java-kotlin support
    
    * feat: refactoring
    
    * feat: move javaapi scala classes
    
    * fix: change import JdbcDsl
    
    * fix: fix Dependencies and readme
    
    * fix: fix Dependencies
    
    ---------
    
    Co-authored-by: a.v.muratova <a.v.muratova@tinkoff.ru>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,14 +5,14 @@ object Dependencies {
   val gatlingVersion = "3.9.0"
 
   lazy val gatlingCore: Seq[ModuleID] = Seq(
-    "io.gatling" % "gatling-core"      % gatlingVersion % "provided",
-    "io.gatling" % "gatling-core-java" % gatlingVersion % "provided",
-  )
+    "io.gatling" % "gatling-core",
+    "io.gatling" % "gatling-core-java",
+  ).map(_ % gatlingVersion % "provided")
 
   lazy val gatling: Seq[ModuleID] = Seq(
-    "io.gatling.highcharts" % "gatling-charts-highcharts",
     "io.gatling"            % "gatling-test-framework",
-  ).map(_ % gatlingVersion % Test)
+    "io.gatling.highcharts" % "gatling-charts-highcharts",
+  ).map(_ % gatlingVersion % "it,test")
 
   lazy val hikari = "com.zaxxer"     % "HikariCP" % "5.0.1" exclude ("org.slf4j", "slf4j-api")
   lazy val h2jdbc = "com.h2database" % "h2"       % "2.1.214" % Test
